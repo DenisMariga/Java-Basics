3a8006e8c6f47a2a638313606a36c53207b8b232
Denis Mariga <denismariga50@gmail.com>
2022-09-02 17:54:41 +0300

    added a casting class

---
+++ Data Types/src/Casting.java
@@ -0,0 +1,16 @@
+package src;
+
+public class Casting {
+public static void main(String[] args) {
+    //    widening casting
+    int number1 = 120;
+    double number2 = number1;
+    System.out.println(number1);
+    System.out.println(number2);
+    double number3 =123.23d;
+    int number4= (int)number3;
+    System.out.println(number3);
+    System.out.println(number4);
+
+}
+}
--- Data Types/src/Main.java
+++ Data Types/src/Main.java
@@ -1,3 +1,5 @@
+package src;
+
 import java.util.Date;
 
 public class Main {
@@ -5,5 +7,9 @@ public class Main {
         System.out.println("Hello world!");
         Date date = new Date();
         System.out.println(date);
+        String message="Good Bye World";
+        System.out.println(message.startsWith("G"));
+        System.out.println(message.endsWith("l"));
+        System.out.println(message.replace("Bye","Day"));
     }
 }

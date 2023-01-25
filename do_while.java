fcb67826aaa5df53fa2a4691db8775175c8fc8a3
Denis Mariga <denismariga50@gmail.com>
2023-01-25 14:56:12 +0300

    learnt about while loop and do while

---
+++ do_while.java
@@ -0,0 +1,14 @@
+/**
+ * do_while
+ */
+public class do_while {
+
+    public static void main(String[] args) {
+        int i = 4;
+        do{
+           System.out.println("executed once");
+           i++;
+        }
+        while(i <= 3);
+    }
+}
---
+++ whiles.java
@@ -0,0 +1,14 @@
+public class whiles {
+   public static void main(String[] args) {
+    int i = 1;
+    while(i <= 5 ){
+        System.out.println(i+ " is less or equal to 5");
+        int j = 1;
+        while (j < 3) {
+            System.out.println(j+ " hey");
+        j++;
+        }
+        i++;
+    }
+   } 
+}

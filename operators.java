5df9d057a4022ea013548688f8d2febe3aaec2fe
Denis Mariga <denismariga50@gmail.com>
2023-01-25 11:55:36 +0300

    learnt about operators

---
+++ operators.java
@@ -0,0 +1,20 @@
+public class operators {
+    public static void main(String[] args) {
+      //  +,-,/,*,&&,||,!
+      int x = 5;
+      if (x > 2 && x >1) {
+        System.out.println("x is big");
+      } else {
+        System.out.println("condition is wrong");
+      }
+      if (x >3 || x>7) {
+        System.out.println("true");
+      } else {
+        System.out.println("false");
+      }
+     boolean result = x>2;
+     System.out.println(!result);
+      }
+    }
+    
+  

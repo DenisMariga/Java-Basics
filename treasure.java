835cf304c982c3bb4ca00b901cffcf489ae0b4bf
Denis Mariga <denismariga50@gmail.com>
2023-01-26 13:16:31 +0300

    created a treasure game

---
+++ treasure.java
@@ -0,0 +1,30 @@
+/**
+ * treasure
+ */
+public class treasure {
+
+    public static void main(String[] args){
+     String optionMove = "left";
+     String optionMove2="Wait";
+     String optionColor ="Yellow";
+     if(optionMove == "left"){
+        System.out.println("You came to a lake.");}
+        else{
+        System.out.println("Game ended");
+    }
+        if (optionMove2 == "Wait") {
+            System.out.println("you arrived in the island unarmed");}
+        else{
+            System.out.println("you got attacked");
+        }
+      
+        if (optionColor == "Yellow"){
+        System.out.println("you win!");
+       }else{
+        System.out.println("game ended");
+      }
+    }}
+     
+       
+    
+   
